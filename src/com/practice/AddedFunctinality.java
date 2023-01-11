3b4f7e5c6ba1ab833943fc91c55bad88181d5d7a
unknown <bordegangotri@gmail.com>
2023-01-11 10:15:17 +0530

    Added Functionality

---
+++ src/com/practice/AddedFunctinality.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class AddedFunctinality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int test=1;
+System.out.println("Tiltle");
+int test2=2;
+System.out.println("Additional hecks");
+	}
+
+}
